6ab2ab35f95b05f0a0aa0009e75762fb4f4325df
Touch_Me <nikitanovikov245@yandex.ru>
2023-12-08 21:23:43 +0000

    теперь идеально

--- counting.cpp
+++ counting.cpp
@@ -140,35 +140,14 @@ int main(int argc, char* argv[]) {
                 }
                 send_message(parent_socket, std::to_string(cur_id) + reply);
             }
-            else if (cmd == "kill") {
+           else if (cmd == "kill") {
                 if (left_id != -1) {
                     send_message(left_socket, std::to_string(left_id) + " kill");
-                    std::string msg = receive_message(left_socket);
-                    if (msg == "OK") {
-                        send_message(parent_socket, "OK");
-                    }
-                  //unbind(left_socket, left_id);
-                 //  disconnect(parent_socket, left_id);
-                 //  disconnect(parent_socket,cur_id);
-
-                    
                 }
                 if (right_id != -1) {
-                    send_message(right_socket, std::to_string(right_id) + " kill");
-                    std::string msg = receive_message(right_socket);
-                    if (msg == "OK") {
-                        send_message(parent_socket, "OK");
-                    }
-                       //unbind(right_socket, right_id);
-                      // disconnect(parent_socket, right_id);
-                      // disconnect(parent_socket, cur_id);
-                   
-                
+                    send_message(right_socket, std::to_string(right_id) + " kill"); 
                 }
-                send_message(parent_socket, "OK");
-                unbind(parent_socket, cur_id);
-                disconnect(parent_socket, cur_id);
-               
+                send_message(parent_socket, "OK");  
                 exit(0);
                 
             }
